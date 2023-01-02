fa5459fc461dc340d1e98cb51c7d5c829efa914e
mb6ockatf <mb6ockatf@relay.firefox.com>
2023-01-02 22:51:55 +0300

    not working

--- main.py
+++ main.py
@@ -36,25 +36,32 @@ def load_image(name: str):
 class Hopalong(pg.sprite.Sprite):
     def __init__(self, *group):
         super().__init__(*group)
-        self.image = load_image(sprite)
+        image = load_image(sprite)
+        self.image = pg.transform.scale(image, (200, 200))
         self.width = self.image.get_width()
         self.height = self.image.get_height()
         self.size = (self.width, self.height)
         self.rect = self.image.get_rect()
-        self.rect.center = 200, 200
+        self.rect.center = screen_height, screen_width // 2
         self.set_physics()
 
     def set_physics(self):
         self.mass = 1
         self.moment = pymunk.moment_for_box(self.mass, self.size)
-        self.body = pymunk.Body(self.mass, self.moment)
-        self.shape = pymunk.Segment(self.body, (200, 200), (3000), 100)
-        self.shape.elasticity = 0
+        self.body = pymunk.Body(body_type=pymunk.Body.KINEMATIC)
+        self.body.moment = self.moment
+        point_a = (self.width // 2 + self.rect.x,
+                    self.height // 2 - self.rect.y)
+        point_b = (self.width + self.rect.x,
+                    self.height + self.rect.y)
+        self.shape = pymunk.Segment(self.body, (point_a), (point_b), 100)
+        self.shape.elasticity = 0.8
         self.shape.friction = 0.5
         space.add(self.body, self.shape)
 
     def update(self):
-        ...
+        self.body.apply_impulse_at_local_point((self.rect.x + self.width // 2,
+        10), (0, 0))
 
 
 
@@ -70,8 +77,6 @@ while running:
                 running = False
     screen.fill(black)
     all_sprites.update()
-    all_sprites.draw(screen)
-    #hopalong.draw(screen)
     all_sprites.draw(screen)
     space.step(1 / FPS)
     space.debug_draw(draw_options)
