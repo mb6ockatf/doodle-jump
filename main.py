946bc93af29f487ed12dec3c0b4c2ae3b3e02444
mb6ockatf <mb6ockatf@relay.firefox.com>
2023-01-06 17:43:35 +0300

    add: tiles spawn

--- main.py
+++ main.py
@@ -16,13 +16,15 @@ screen_height = info.current_h
 pg.display.set_caption("DOODLE JUMP")
 black = pg.Color("black")
 white = pg.Color("white")
+difficulty = 6
+shift = 0
+step = 0
 running = True
 
-
-def load_image(name: str):
+def load_image(name: str) -> pg.Surface:
     fullname = os.path.join('data', name)
     if not os.path.isfile(fullname):
-        print(f"Файл с изображением '{fullname}' не найден")
+        print(f"file '{fullname}' not found")
         pg.quit()
     image = pg.image.load(fullname)
     image.set_colorkey(black)
@@ -31,23 +33,39 @@ def load_image(name: str):
 
 
 class Tile(pg.sprite.Sprite):
-    def __init__(self, *group):
-        super().__init__(*group)
+    def __init__(self, group: pg.sprite.Group, hero: pg.sprite.Sprite):
+        super().__init__(group)
         image = load_image("tile.png")
-        self.image = pg.transform.scale(image, (310, 70))
+        self.image = pg.transform.scale(image, (155, 35))
         self.width = self.image.get_width()
         self.height = self.image.get_height()
         self.size = (self.width, self.height)
         self.rect = self.image.get_rect()
-        self.rect.center = self.generate(), screen_height - 200
+        center_height = self.generate_height()
+        center_width = self.generate_width(hero)
+        ### TRIANGLE
+        self.rect.center = self.generate_width(hero), self.generate_height()
         self._edges = None
+        self.is_in_progress = False
+        self.progress_distance = None
 
-    def generate(self):
-        x = randint(0, screen_width)
-        return x
+    def generate_width(self, correction_by: pg.sprite.Sprite) -> int:
+        radius = correction_by.move_length
+        minimal = correction_by.rect.x - radius
+        minimal = round(minimal)
+        if minimal < 0:
+            minimal = 0
+        maximal = correction_by.rect.x + radius
+        maximal = round(maximal)
+        if maximal > screen_width:
+            maximal = screen_width
+        return randint(minimal, maximal)
+
+    def generate_height(self) -> int:
+        return randint(screen_height // 2 - 200, screen_height // 5 * 3)
 
     @property
-    def edges(self):
+    def edges(self) -> tuple:
         if not self._edges:
             left_edge = self.rect.x - self.width // 2
             right_edge = self.rect.x + self.width // 2
@@ -56,78 +74,79 @@ class Tile(pg.sprite.Sprite):
 
 
 class Hopalong(pg.sprite.Sprite):
-    def __init__(self, *group):
+    def __init__(self, *group: pg.sprite.Group):
         super().__init__(*group)
-        self.jump_height = screen_height // 2
-        self.move_length = screen_width // 6
+        self.jump_height = screen_height // 3 * 2
+        self.move_length = screen_width // 2
         self.is_moving_up = None
         self.left_to_move_up = self.jump_height
         self.is_moving_right = None
         self.left_to_move_right = self.move_length
         self.tile = None
         image = load_image(sprite)
-        self.image = pg.transform.scale(image, (200, 200))
+        self.image = pg.transform.scale(image, (100, 100))
         self.width = self.image.get_width()
         self.height = self.image.get_height()
         self.size = (self.width, self.height)
         self.rect = self.image.get_rect()
         self.rect.center = screen_height, screen_width // 2
 
-    def update(self, group):
+    def update(self, group: pg.sprite.Group):
         for _ in range(10):
-          self.move(group)
+            self.move(group)
 
-    def move(self, group):
-        if not self.is_moving_up:
-            if pg.sprite.spritecollideany(self, group):
-                for element in group:
-                    if self.rect.y - element.rect.y == -199:
-                        self.left_to_move_up = self.jump_height
-                        self.is_moving_up = None
-                        self.tile = element
-                        break
+    def move(self, group: pg.sprite.Group):
+        is_still = not self.is_moving_up
+        is_colliding = self.is_having_collision(group)
+        if is_still and is_colliding:
+            self.left_to_move_up = self.jump_height
+            self.is_moving_up = True
+            self.tile = is_colliding
+        if self.is_moving_right is True:
+            self.rect.x += 10
+        elif self.is_moving_right is False:
+            self.rect.x -= 10
         if self.is_moving_right is not None:
-            if self.is_moving_right:
-                self.rect.x += 1
-                if self.tile:
-                    edge = self.tile.edges[1]
-                    if edge < self.rect.x:
-                        self.tile = None
-                        self.is_moving_up = False
-            else:
-                self.rect.x -= 1
-                if self.tile:
-                    edge = self.tile.edges[0]
-                    if edge > self.rect.x:
-                        self.tile = None
-                        self.is_moving_up = False
-            self.left_to_move_right -= 1
-        if not self.left_to_move_right:
             self.is_moving_right = None
-            self.left_to_move_right = self.move_length
-        if self.is_moving_up is not None:
-            if self.is_moving_up:
+        if self.tile:
+            edge = self.tile.edges
+            if edge[0] > self.rect.x or edge[1] < self.rect.x:
+                self.tile = None
+                self.is_moving_up = False
+        if self.is_moving_up is True:
+            if self.rect.y > 0:
                 self.rect.y -= 1
-                self.left_to_move_up -= 1
-            else:
-                self.rect.y += 1
+            self.left_to_move_up -= 1
+        elif self.is_moving_up is False:
+            self.rect.y += 1
         if not self.left_to_move_up:
             if self.is_moving_up:
                 self.is_moving_up = False
             else:
-                self.is_moving_up = None
+                self.is_moving_up = True
             self.left_to_move_up = self.jump_height
+        print("left_to_move_up", self.left_to_move_up)
 
     def is_alive(self):
         if self.rect.y > screen_height:
             return False
         return True
 
+    def is_having_collision(self, group: pg.sprite.Group) -> pg.sprite.Sprite:
+        for sprite in group:
+            if not (sprite.edges[0] < self.rect.x < sprite.edges[1]):
+                continue
+            difference_height = sprite.rect.y - self.rect.y
+            print(difference_height)
+            if difference_height != 20:
+                continue
+            return sprite
+
 
 all_sprites = pg.sprite.Group()
 hopalong = Hopalong(all_sprites)
 tiles = pg.sprite.Group()
-tile = Tile(tiles)
+tile = Tile(tiles, hopalong)
 while running:
     for event in pg.event.get():
         if event.type == pg.QUIT:
@@ -145,8 +164,27 @@ while running:
         hopalong.is_moving_right = False
     if hopalong.is_alive() is False:
         running = False
+    all_sprites.update(tiles)
+    tiles.update()
+    measurer = hopalong.tile
+    if measurer and not shift:
+        value = screen_height - 200 - measurer.rect.y
+        if value > 0:
+            shift = value
+    if shift:
+        if not step:
+            step = shift // 10
+        round_step = round(step)
+        for entity in tiles:
+            entity.rect.y += round_step
+        shift -= step
+        if shift <= 0:
+            newcomers = randint(1, 2)
+            for _ in range(newcomers):
+                tile = Tile(tiles, hopalong)
+            shift = 0
+    hopalong.tile = None
     screen.fill(black)
-    turn_result = all_sprites.update(tiles)
     all_sprites.draw(screen)
     tiles.draw(screen)
     pg.display.flip()
