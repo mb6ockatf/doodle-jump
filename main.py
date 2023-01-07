6b47d4bf176486815c3ae740a60f5a3fa965feb1
mb6ockatf <mb6ockatf@relay.firefox.com>
2023-01-08 01:09:10 +0300

    sortta work now

--- main.py
+++ main.py
@@ -2,21 +2,22 @@
 import sys
 import os
 from random import randint
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 import pygame
 
 pygame.init()
 FPS = 70
 clock = pygame.time.Clock()
-pygame.mouse.set_visible(False)
 screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 info = pygame.display.Info()
 screen_width = info.current_w
 screen_height = info.current_h
+screen_height_div_five = screen_height // 5
 pygame.display.set_caption("DOODLE JUMP")
 black = pygame.Color("black")
 white = pygame.Color("white")
-difficulty = 6
-running = True
+font = pygame.font.SysFont(None, 144)
+score = 0
 
 
 def load_image(name: str) -> pygame.Surface:
@@ -29,73 +30,62 @@ def load_image(name: str) -> pygame.Surface:
     image.convert_alpha()
     return image
 
+def round_by_ten(number: int) -> int:
+    number = round(number, -1)
+    number= int(number)
+    return number
 
-class Tile(pygame.sprite.Group):
-    def __init__(self,
-                 is_broken: bool,
-                 group: pygame.sprite.Group,
-                 player_jump: int,
-                 player_x: int):
+
+class Tile(pygame.sprite.Sprite):
+    def __init__(self, is_broken: bool, group: pygame.sprite.Group,
+                 player: pygame.sprite.Sprite):
         super().__init__(group)
         self.is_broken = is_broken
         self.is_falling = False
+        image_name = "tile.png"
         if self.is_broken:
             image_name = "broken_tile.png"
-        else:
-            image_name = "tile.png"
-        image = load_image(image_name)
-        self.image = pygame.transform.scale(image, (155, 35))
+        self.image = load_image(image_name)
+        self.image = pygame.transform.scale(self.image, (155, 35))
         self.width = self.image.get_width()
-        self.height = self.image.get_height()
-        self.size = (self.width, self.height)
         self.rect = self.image.get_rect()
-        center_height = self.generate_height()
-        center_width = self.generate_width(player_jump, player_x)
-        self.rect.center = (center_width, center_height)
+        hight_calc_data = (player.jump_height, player.height, player.rect.y)
+        self.rect.y = self.generate_height(*hight_calc_data)
+        x_value = randint(1, screen_width - 1)
+        self.rect.x = round_by_ten(x_value)
         self._edges = None
-        self.rect.y = center_height
 
-    def generate_width(self, correction_radius: int, correction_x: int) -> int:
-        minimal = correction_x - correction_radius
-        minimal = round(minimal)
+    def generate_height(self, radius: int, edge: int, minimal_y: int) -> int:
+        minimal = minimal_y - radius + edge * 1.5
+        maximal = minimal_y + radius - edge * 1.5
         if minimal < 0:
-            minimal = 0
-        maximal = correction_x + correction_radius
-        maximal = round(maximal)
-        if maximal > screen_width:
-            maximal = screen_width
-        return randint(minimal, maximal)
-
-    def generate_height(self) -> int:
-        value = randint(screen_height // 4, screen_height // 3 * 2)
-        value = int(round(value, -1))
+            minimal = edge
+        elif maximal > screen_height:
+            maximal = screen_height - edge
+        value = randint(minimal, maximal)
+        value = round_by_ten(value)
         return value
 
     @property
     def edges(self) -> tuple:
         if not self._edges:
-            left_edge = self.rect.x - self.width // 2
-            right_edge = self.rect.x + self.width // 2
-            self._edges = (left_edge, right_edge)
+            x, half_width = self.rect.x, self.width // 2
+            self._edges = (x - half_width, x + half_width)
         return self._edges
 
-"""
     def update(self):
         if self.is_broken and self.is_falling:
-            self.fall()
-
-    def fall(self):
-        self.rect.y += 50
-        if self.rect.y > screen_height:
+            self.rect.y += 50
+        if self.rect.y >= screen_height:
             self.kill()
-"""
 
 
 class Hopalong(pygame.sprite.Sprite):
-    def __init__(self, name: str, group: pygame.sprite.Group):
+    def __init__(self, group: pygame.sprite.Group, name: str):
         super().__init__(group)
-        self.jump_height = int(round(screen_height // 3 * 2, -1))
-        self.move_length = screen_width // 2
+        self.jump_height = screen_height // 3 * 2
+        self.jump_height = round_by_ten(self.jump_height)
+        self.move_length = screen_width
         self.is_moving_up = None
         self.left_to_move_up = self.jump_height
         self.is_moving_right = None
@@ -112,135 +102,145 @@ class Hopalong(pygame.sprite.Sprite):
             sprite_right = pygame.transform.scale(sprite_right, size)
             self.image_storage[self.name + "_left"] = sprite_left
             self.image_storage[self.name + "_right"] = sprite_right
-            if self.image is None:
-                self.image = self.image_storage[self.name + "_left"]
+            self.image = self.image_storage[self.name + "_left"]
         self.width = self.image.get_width()
         self.height = self.image.get_height()
         self.size = (self.width, self.height)
         self.rect = self.image.get_rect()
         self.rect.center = screen_height, screen_width // 2
 
-    def update(self, group: pygame.sprite.Group, shift: int):
-        if self.is_facing_right is True:
-            self.image = self.image_storage[self.name + "_right"]
+    def update(self, group: pygame.sprite.Group) -> bool:
+        if self.is_facing_right:
+            image_name = self.name + "_right"
         else:
-            self.image = self.image_storage[self.name + "_left"]
-        shift = self.move(group, shift)
-        return shift
-
-    def move(self, group: pygame.sprite.Group, shift: int):
+            image_name = self.name + "_left"
+        self.image = self.image_storage[image_name]
+        shifted = False
         is_still = not self.is_moving_up
-        is_colliding = self.is_having_collision(group)
-        if is_still and is_colliding:
+        colliding_object = self.collide(group)
+        if is_still and colliding_object:
+            colliding_object.is_falling = colliding_object.is_broken
             self.left_to_move_up = self.jump_height
             self.is_moving_up = True
-            self.on_tile_edges = is_colliding.edges
-            if shift == 0:
-                shifting_height = self.count_shift(is_colliding)
-                if shifting_height > 0:
-                    shift = shifting_height
+            shifted = True
         if self.is_moving_right is True:
-            self.rect.x += 18
+            self.rect.x += 25
             self.is_facing_right = True
-            if self.rect.x > screen_width:
-                self.rect.x = 0
         elif self.is_moving_right is False:
-            self.rect.x -= 18
+            self.rect.x -= 25
             self.is_facing_right = False
-            if self.rect.x < 0:
-                self.rect.x = screen_width
-        if self.is_moving_right is not None:
-            self.is_moving_right = None
-        if self.on_tile_edges:
-            edges = self.on_tile_edges
-            if edges[0] > self.rect.x or edges[1] < self.rect.x:
-                self.on_tile_edges = None
-                self.is_moving_up = False
+        self.rect.x %= screen_width
+        self.is_moving_right = None
         if self.is_moving_up is True:
             if self.rect.y > 0:
                 self.rect.y -= 10
             self.left_to_move_up -= 10
         elif self.is_moving_up is False:
             self.rect.y += 10
+        if self.rect.y % 10 != 0:
+            self.rect.y = round_by_ten(self.rect.y)
         if not self.left_to_move_up:
-            if self.is_moving_up:
-                self.is_moving_up = False
-            else:
-                self.is_moving_up = True
+            self.is_moving_up = not self.is_moving_up
             self.left_to_move_up = self.jump_height
-        return shift
+        return shifted
 
-    def is_alive(self):
+    def is_alive(self) -> bool:
         if self.rect.y > screen_height:
             return False
         return True
 
-    def is_having_collision(self, group: pygame.sprite.Group) -> pygame.sprite.Sprite:
+    def collide(self, group: pygame.sprite.Group):
         for sprite in group:
-            if not (sprite.edges[0] < self.rect.x < sprite.edges[1]):
+            if not sprite.edges[0] < self.rect.x < sprite.edges[1]:
                 continue
             if sprite.rect.y - self.rect.y != 80:
                 continue
             return sprite
 
-    def count_shift(self, platform: pygame.sprite.Sprite):
-        measurer = platform.rect.y
-        value = round(screen_height - 200 - measurer, -1)
-        value = int(value)
-        return value
-
 
 sprites = ["bird", "sprout"]
-player_group = pygame.sprite.Group()
 sprite_name = sprites[randint(0, 1)]
-hopalong = Hopalong(sprite_name, player_group)
+player_group = pygame.sprite.Group()
+hopalong = Hopalong(player_group, sprite_name)
 tiles_group = pygame.sprite.Group()
-tile = Tile(False, tiles_group, hopalong.move_length, hopalong.rect.x)
-print(screen_width, screen_height)
-print(tile.rect.x, tile.rect.y)
+Tile(False, tiles_group, hopalong)
 SHIFT = 0
 STEP =  0
-while running:
+score_position = (screen_width // 10 * 9, screen_height // 10)
+is_paused = True
+is_running = False
+is_opened = True
+while is_opened:
+    screen.fill(black)
+    pygame.display.update()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
+            is_opened = False
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
-                running = False
-            if event.key == pygame.K_UP:
-                if hopalong.is_moving_up is None:
-                    hopalong.is_moving_up = True
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_RIGHT]:
-        hopalong.is_moving_right = True
-    elif keys[pygame.K_LEFT]:
-        hopalong.is_moving_right = False
-    if hopalong.is_alive() is False:
-        running = False
-    is_shifting = player_group.update(tiles_group, SHIFT)
-    if is_shifting:
-        SHIFT = is_shifting
-        STEP = SHIFT // 10
-        newcomers = randint(1, 3)
-        for _ in range(newcomers):
-            correction_data = (hopalong_move_length, hopalong.rect.x)
-            tile = Tile(False, tiles_group, *correction_data)
-        broken_newcomers = randint(1, 2)
-        for _ in range(broken_newcomers):
-            correction_data = (hopalong_move_length, hopalong.rect.x)
-            tile = Tile(True, tiles_group, *correction_data)
-    if SHIFT <= 0:
-        shift = 0
-    else:
-        for entity in tiles_group:
-            entity.rect.y += STEP
-        SHIFT -= STEP
-    tiles_group.update()
-#    screen.fill(black)
-    player_group.draw(screen)
-    tiles_group.draw(screen)
-    pygame.display.flip()
-    pygame.display.update()
-    clock.tick(FPS)
+                is_opened = False
+            if event.key in (pygame.K_UP, pygame.K_SPACE):
+                is_running = True
+                pygame.mouse.set_visible(False)
+    while is_running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                is_running = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    is_running = False
+                    pygame.mouse.set_visible(True)
+                if event.key in (pygame.K_UP, pygame.K_SPACE):
+                    if hopalong.is_moving_up is None:
+                        hopalong.is_moving_up = True
+                    if not is_paused:
+                        is_paused = True
+                    else:
+                        is_paused = False
+        keys = pygame.key.get_pressed()
+        score_img = font.render(str(score), True, white)
+        if not is_paused:
+            if keys[pygame.K_RIGHT]:
+                hopalong.is_moving_right = True
+            elif keys[pygame.K_LEFT]:
+                hopalong.is_moving_right = False
+            if hopalong.is_alive() is False:
+                is_running = False
+            tiles_group.update()
+            is_shifting = hopalong.update(tiles_group)
+            if not SHIFT and is_shifting:
+                heights = [element.rect.y for element in tiles_group]
+                heights.sort()
+                SHIFT = round_by_ten(screen_height_div_five)
+                STEP = SHIFT // 10
+                STEP = round_by_ten(STEP)
+            if SHIFT > 0:
+                for entity in tiles_group:
+                    entity.rect.y += STEP
+                SHIFT -= STEP
+                if SHIFT <= 0:
+                    SHIFT = 0
+                    Tile(False, tiles_group, hopalong)
+                    broken_newcomers = randint(0, 2)
+                    for _ in range(broken_newcomers):
+                        Tile(True, tiles_group, hopalong)
+                    score += 10
+        screen.fill(black)
+        player_group.draw(screen)
+        tiles_group.draw(screen)
+        screen.blit(score_img, score_position)
+        pygame.display.flip()
+        pygame.display.update()
+        clock.tick(FPS)
+file_exists = open("record.bin", "ab").close()
+with open("record.bin", "rb") as bytestream:
+    current_record = bytestream.read().decode("utf-8")
+    if current_record == "":
+        current_record = 0
+    current_record = int(current_record)
+if score > current_record:
+    new_record = str(score).encode("utf-8")
+    with open("record.bin", "wb") as file:
+        file.write(new_record)
 pygame.quit()
 
