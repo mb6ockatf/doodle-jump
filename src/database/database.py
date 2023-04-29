3148747b41775db5bb8ad0c879668b5dfb49708b
mb6ockatf <mdddmmmm@yandex.ru>
2023-04-30 00:18:56 +0300

    refactor (#32)
    * refactor
    
    * fix: score out of display
    
    resolves #18
    
    ---------
    
    Signed-off-by: mb6ockatf <mdddmmmm@yandex.ru>

--- src/database/database.py
+++ src/database/database.py
@@ -1,36 +1,21 @@
-"""
-Class to work with database
-"""
 from os import path, getcwd
 from sqlite3 import connect
 
 
 class DatabaseConnection:
-    """
-    handle DB connection and queries execution
-    """
     def __init__(self):
         db_path = path.realpath(path.join(getcwd(), "records.db"))
         if not path.isfile(db_path):
             open(db_path, "w", encoding="utf-8").close()
         self.connection = connect(db_path)
-        query = """
-        CREATE TABLE IF NOT EXISTS main (
-            nickname TEXT UNIQUE NOT NULL,
-            record INTEGER UNSIGNED DEFAULT 0);
-        """
+        query = """CREATE TABLE IF NOT EXISTS main (
+        nickname TEXT UNIQUE NOT NULL, record INTEGER UNSIGNED DEFAULT 0);"""
         self._execute(query, None)
 
     def close(self):
-        """
-        Close database connection
-        """
         self.connection.close()
 
     def _execute(self, query: str, data) -> list:
-        """
-        Execute SQL query with values
-        """
         cursor = self.connection.cursor()
         if data:
             cursor.execute(query, data)
@@ -42,20 +27,11 @@ class DatabaseConnection:
         return result
 
     def insert(self, nickname: str, value: int):
-        """
-        insert new record into database
-        """
         data = {"nickname": nickname, "value": value}
-        query = """
-        SELECT record FROM main
-        WHERE nickname = :nickname;
-        """
+        query = """SELECT record FROM main WHERE nickname = :nickname;"""
         result = self._execute(query, data)
-        query = """
-        INSERT OR REPLACE INTO main
-        (nickname, record)
-        VALUES (:nickname, :value);
-        """
+        query = """INSERT OR REPLACE INTO main (nickname, record)
+        VALUES (:nickname, :value);"""
         answer = None
         write = False
         if result == []:
@@ -67,14 +43,6 @@ class DatabaseConnection:
         return answer
 
     def select(self, nickname: str):
-        """
-        insert new record into database
-        """
         data = {"nickname": nickname}
-        query = """
-        SELECT record
-        FROM main
-        WHERE nickname = :nickname;
-        """
-        result = self._execute(query, data)
-        return result
+        query = """SELECT record FROM main WHERE nickname = :nickname;"""
+        return self._execute(query, data)
--- src/sprites/tile.py
+++ src/sprites/tile.py
@@ -1,20 +1,9 @@
-"""
-tile sprite class
-"""
 from random import randint
 import pygame
 from tools import round_by_five, load_image
 
 
 class Tile(pygame.sprite.Sprite):
-    """
-    Tile's sprite and behaviour
-    tile is an object of platform where player can land
-    there are two types of tiles: normal and broken
-    normal ones do nothing when they're hit
-    broken ones start falling, so they can be stepped on only once
-    currently, tiles are not moving horizontally
-    """
     def __init__(self, is_broken: bool, group: pygame.sprite.Group,
                  player_height: tuple, screen_sizes: tuple):
         super().__init__(group)
@@ -35,19 +24,6 @@ class Tile(pygame.sprite.Sprite):
         self.edges = (x - half_width, x + half_width)
 
     def generate_height(self, radius: int, edge: int, minimal_y: int) -> int:
-        """
-        Provides some random height value for tile to spawn
-        radius is supposed to be player's jump height
-        edge is player sprite picture's height
-        minimal_y is player's current y
-        Thus, tile has a range of values from
-        minimal_y - radius + edge * 1.5
-        to
-        minimal_y + radius - edge * 1.5
-        what makes sure that it's always possible to land on it
-
-        return: int random height value
-        """
         minimal = round(minimal_y - radius + edge * 1.5)
         maximal = round(minimal_y + radius - edge * 1.5)
         if minimal < 0:
@@ -55,14 +31,9 @@ class Tile(pygame.sprite.Sprite):
         elif maximal > self.screen_sizes[1]:
             maximal = self.screen_sizes[1] - edge
         value = randint(minimal, maximal)
-        value = round_by_five(value)
-        return value
+        return round_by_five(value)
 
     def update(self):
-        """
-        Fall if tile's broken and it has been hit
-        disappear if it is out of the screen
-        """
         if self.is_broken and self.is_falling:
             self.rect.y += 50 #type:ignore
         if self.rect.y >= self.screen_sizes[1]: #type:ignore
